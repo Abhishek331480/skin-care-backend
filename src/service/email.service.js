a27426c03155b4580b62baa627a69d1d85429532
Abhishek331480 <abhishekthakur412002.com>
2026-06-10 18:29:42 +0530

    add email error logging

--- src/service/email.service.js
+++ src/service/email.service.js
@@ -1,20 +1,48 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async ({ to, subject, html }) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: `"SkinCare Store" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html,
+//   });
+// };
+
+// module.exports = sendEmail;
+
 const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"SkinCare Store" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    await transporter.sendMail({
+      from: `"SkinCare Store" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent successfully to:", to);
+  } catch (error) {
+    console.error("Email send error:", error.message);
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
