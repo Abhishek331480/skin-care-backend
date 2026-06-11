854d447cb2d3a72fb4664f907f66d9f0dd18e8b5
Abhishek331480 <abhishekthakur412002.com>
2026-06-11 14:15:28 +0530

    fix email timeout  order creation

--- src/service/email.service.js
+++ src/service/email.service.js
@@ -24,14 +24,16 @@ const nodemailer = require("nodemailer");
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+     transporter.sendMail({
       from: `"SkinCare Store" <${process.env.EMAIL_USER}>`,
       to,
       subject,
